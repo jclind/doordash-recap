57a5bb8dcf69282d6d73a322fec6a24826eea16f
Jesse Lind <Jesseclind@gmail.com>
2023-12-05 00:21:43 -0500

    build: add react-helmet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,66 +1,73 @@
 import React from 'react'
-import './Home.scss'
 import { Link } from 'react-router-dom'
 import { tutorialData } from '../assets/data/tutorialData'
+import { Helmet } from 'react-helmet'
+import './Home.scss'
 
 const Home = () => {
   return (
-    <div className='home-page page'>
-      <div className='content'>
-        <div className='header'>
-          <h1>
-            Your DoorDash Deliveries
-            <br />
-            Yearly Recap
-          </h1>
-          <p>Like Spotify Wrapped, but for DoorDash drivers.</p>
-        </div>
+    <>
+      <Helmet>
+        <title>My Title</title>
+        <link rel='canonical' href='http://mysite.com/example' />
+      </Helmet>
+      <div className='home-page page'>
+        <div className='content'>
+          <div className='header'>
+            <h1>
+              Your DoorDash Deliveries
+              <br />
+              Yearly Recap
+            </h1>
+            <p>Like Spotify Wrapped, but for DoorDash drivers.</p>
+          </div>
 
-        <div className='options'>
-          <div className='top-btns'>
-            <Link to='/tutorial' className='primary tutorial'>
-              How To Get Your Data
-            </Link>
+          <div className='options'>
+            <div className='top-btns'>
+              <Link to='/tutorial' className='primary tutorial'>
+                How To Get Your Data
+              </Link>
+              <Link
+                to={`/tutorial?page=${tutorialData.length}`}
+                className='primary upload-data'
+              >
+                Upload Data
+              </Link>
+            </div>
             <Link
-              to={`/tutorial?page=${tutorialData.length}`}
-              className='primary upload-data'
+              to='/recap?useExampleData=true'
+              className='btn-no-styles example'
             >
-              Upload Data
+              Show Me An Example
             </Link>
           </div>
-          <Link
-            to='/recap?useExampleData=true'
-            className='btn-no-styles example'
-          >
-            Show Me An Example
-          </Link>
         </div>
-      </div>
 
-      <footer>
-        <section className='top'>
-          <div className='text'>
-            Created by{' '}
-            <a
-              href='https://www.jesselind.com/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Jesse Lind
-            </a>{' '}
-            with React & Typescript
-          </div>
-        </section>
-        <section className='middle'>
-          <Link to='/terms' className='legal-link'>
-            Terms & Conditions
-          </Link>
-          <Link to='/privacy' className='legal-link'>
-            Privacy Policy
-          </Link>
-        </section>
-      </footer>
-    </div>
+        <footer>
+          <section className='top'>
+            <div className='text'>
+              Created by{' '}
+              <a
+                href='https://www.jesselind.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Jesse Lind
+              </a>{' '}
+              with React & Typescript
+            </div>
+          </section>
+          <section className='middle'>
+            <Link to='/terms' className='legal-link'>
+              Terms & Conditions
+            </Link>
+            <Link to='/privacy' className='legal-link'>
+              Privacy Policy
+            </Link>
+          </section>
+        </footer>
+      </div>
+    </>
   )
 }
 
